0b6f4672ea30e52a5bd5d58fd39a7a119acbc6ac
Rafa130206 <rafabompadre@gmail.com>
2025-10-16 19:45:26 -0300

    Classes do Models criadas

---
+++ WebApplication1/Models/Note.cs
@@ -0,0 +1,13 @@
+﻿namespace SafeScribe.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string content { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public int UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
---
+++ WebApplication1/Models/RoleNames.cs
@@ -0,0 +1,9 @@
+﻿namespace SafeScribe.Models
+{
+    public static class RoleNames
+    {
+        public const string Reader = "Leitor";
+        public const string Editor = "Editor";
+        public const string Admin = "Admin";
+    }
+}
